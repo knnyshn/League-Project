5b7adc53d3c45bbf1e6397a62b5b6e9044f3c209
Kenny <knnyshn@gmail.com>
2023-02-21 10:37:40 -0500

    test

--- api/index.js
+++ api/index.js
@@ -1,25 +1,36 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import lifecycle from './middleware/lifecycle.js'
 
 const app = express()
+
+const todoSchema = new mongoose.Schema({
+  text: String
+})
+
+const Todo = mongoose.model('Todo', todoSchema)
 
 app.use(lifecycle({
   async setup() {
     console.log('Before handler')
     // Put your database connection here. e.g.
-    // await mongoose.connect(process.env.DATABASE_URL)
+    // @ts-ignore
+    await mongoose.connect(process.env.DATABASE_URL)
   },
   async cleanup() {
     console.log('After handler')
     // Put your database disconnection here. e.g.
-    // await mongoose.disconnect()
+    await mongoose.disconnect()
   }
 }))
 
 // Feel free to use a router and move this elsewhere.
 app.get('/api', async (req, res) => {
+  await Todo.insertMany([{ text: (new Date()).toISOString() }])
+  const todos = await Todo.find()
+
   console.log(process.env.DATABASE_URL)
-  res.json({ message: 'Hello World' })
+  res.json({ message: 'Hello World', todos })
 })
 
 // Don't use app.listen. Instead export app.
